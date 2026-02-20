ef6a772a576b1ad9ca12b3073c5fcfd156f28dc2
Nyco's_burner <nycolas.galvao@escola.pr.gov.br>
2026-02-20 08:48:47 -0300

    Add files via upload

---
+++ atividade1.py
@@ -0,0 +1,27 @@
+### Atividade 1 - Nivelamento ###
+# Objetivo: Criar sua autodescrição utilizando variáveis e função print().
+# 1 - Pesquise o que é autodescrição e para quê ela serve.
+# 2 - Crie as seguintes variáveis:
+nome = "Nycolas"
+idade = 17
+altura = 1.81
+genero = "masculino"
+cor_de_pele = "branco"
+cor_de_cabelo = "castanho"
+tamanho_cabelo = "grande"
+estilo_cabelo = "amarado"
+# 3 - Crie 2 variáveis: uma armazena o curso que você faz, a outra armazena o seu hobby favorito.
+hobby_favorito1 = "jogar jogos"
+curso = "desenvolvimento de sistemas"
+# 4 - Utilizando a função print(), mostre o texto completo da sua autodescrição, utilizando as variáveis que você criou!
+# Exemplo: "Olá, meu nome é Fulano, tenho 999 anos e 1.90 de altura... (continue até usar todas as variáveis)"
+print("Olá meu nome é", nome,"tenho", idade,"anos e tenho", altura,"de altura, sou do genero", genero,"minha cor de pele sendo", cor_de_pele,"e o meu cabelo sendo", cor_de_cabelo,",", tamanho_cabelo,"e", estilo_cabelo,". E os meus hobby's favoritos são", hobby_favorito1,"e eu tambem faço curso de", curso)
+
+## Forma de avaliação: ##
+# 0,5 ponto = Preenchimento correto das variáveis do item 2
+# 0,5 ponto = Criação das variáveis do item 3
+# 1,0 ponto = Coerência do texto e uso correto das variáveis na função print()
+# TOTAL: 2,0 pontos
+
+## Entrega: ##
+# O aluno deve criar um novo repositório no GitHub, com o nome "1TRI-DesenvolvimentoSistemas" e realizar upload do arquivo .py.
