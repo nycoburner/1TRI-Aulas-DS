6d023a2db30efea19259c768a4214546debbf9bb
Nyco's_burner <nycolas.galvao@escola.pr.gov.br>
2026-02-20 08:04:04 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,11 @@
+print("Olá mundo!")
+#As minhas caracteristicas
+nome = "Nycolas"
+cor_de_pele = "branco"
+genêro = "Masculino"
+idade = 17
+sonho = "ir para o Japão"
+#Usar o print com variáveis
+print("meu nome é", nome, "minha cor de pele é", cor_de_pele,"o meu genêro é", genêro,"e tenho", idade,"anos e gostari de", sonho)
+#F-string 
+print(f'Olá, meu nome é, {nome},"minha idade é, {idade}')
