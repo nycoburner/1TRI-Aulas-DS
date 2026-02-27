a6495ba4eb2b18da373e49b537e2f7af96f88634
Nyco's_burner <nycolas.galvao@escola.pr.gov.br>
2026-02-27 08:58:56 -0300

    Add files via upload

---
+++ DS/while.py
@@ -0,0 +1,6 @@
+contador = 1
+# enquanto ( se condição for verdadeira): -> executa todo
+# Enquanto ( se condição for falsa): -> para execução
+while (contador <= 100):
+ print(contador)
+ contador = contador + 1
