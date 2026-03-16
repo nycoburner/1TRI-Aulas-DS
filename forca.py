95f118c42d027547924f3894689510d1b393883d
Nyco's_burner <nycolas.galvao@escola.pr.gov.br>
2026-03-16 09:42:45 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -1,25 +1,42 @@
 palavra = "KENNEDY"
-letrasAcertadas = ["_","_","_","_","_","_","_"]
+letras_acertadas = ["_", "_", "_", "_", "_", "_", "_"]
 acertou = False
+enforcou = False
+limite_tentativas = len(palavra) + 6
+tentativa = 1
 
 def mostrar_letras_acertadas():
-    for letra in letrasAcertadas:
+    for letra in letras_acertadas:
         print(letra, end=" ")
 
-print("Letras acertadas:")
-
-print("Tente advinha adivinhar a palavra secreta: ")
-while (not acertou):
+print("Tente adivinhar a palavra secreta: ")
+while(not acertou and not enforcou):
+    # mostrar as letras acertadas
     mostrar_letras_acertadas()
-
-    print(" ")
+    
+    print("")
     chute = input("Digite uma letra: ")
     indice = 0
+
     for letra in palavra:
         if chute.upper() == letra:
-            letrasAcertadas [indice] = letra
-        indice = indice +1
-    if letrasAcertadas.count("_") == 0:
-        print("Parabens, você acertou!")
+            letras_acertadas[indice] = letra
+        indice = indice + 1
+
+    if tentativa == limite_tentativas:
+        print ("você perdeu :( A palavra era ", palavra)
+        enforcou = True
+
+    if letras_acertadas.count("_") == 0:
+        print("Parabéns, você acertou a palavra secreta!")
         mostrar_letras_acertadas()
         acertou = True
+    tentativa = (tentativa + 1)
+
+# Tabela VERDADE
+# Quero fazer um suco de banana e/ou maçã
+#     E    |     OU
+# v v = v  |  V V = V
+# V F = F  |  V F = V
+# F V = F  |  V F = V
+# F F = F  |  F F = F
